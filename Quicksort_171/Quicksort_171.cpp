11d3578c147d4e2b6de8e3c8f15ec270e8d6c8db
arifrahmadi23 <muhammad231arif@gmail.com>
2023-03-31 10:54:30 +0700

    membuat prosedur input

--- Quicksort_171/Quicksort_171.cpp
+++ Quicksort_171/Quicksort_171.cpp
@@ -6,3 +6,15 @@ int arr[20];
 int cmp_count = 0; // number of comparasion
 int mov_count = 0; // nummber of movement
 int n;
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n Maksimum panjang array adalah 20" << endl;
+	}
+}
